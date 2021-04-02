f6542172d2dbab9f112b63ffcf3b5a99d82d0d78
youzhilong <73928124+youzhilong@users.noreply.github.com>
2021-04-02 16:00:52 +0800

    初始化pom

---
+++ src/main/java/com/youhopefly/smp/SmpApplicathon.java
@@ -0,0 +1,17 @@
+package com.youhopefly.smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc: 类描述:
+ * @author: youzhilong
+ * @createDate: 2021/4/2 15:56
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class SmpApplicathon {
+    public static void main(String[] args) {
+        SpringApplication.run(SmpApplicathon.class, args);
+    }
+}
